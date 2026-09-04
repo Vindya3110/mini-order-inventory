a265abda105f4e2d2d96020f9dcef6278d8fc9f6
Shaamam <shaamam@users.noreply.github.com>
2026-09-04 18:59:03 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/vindya/mini_order_inventory/MiniOrderInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.vindya.mini_order_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniOrderInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniOrderInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vindya/mini_order_inventory/MiniOrderInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vindya.mini_order_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniOrderInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
